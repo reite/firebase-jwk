c8132ffc94c659308e3a6187f23e128f2737c5fc
Christian Reite <csreite@gmail.com>
2018-03-03 18:46:39 +0700

    Export Store in Firebase.JWK

--- src/Firebase/JWK.hs
+++ src/Firebase/JWK.hs
@@ -1,9 +1,11 @@
 
 module Firebase.JWK (
+    module Firebase.JWK.Store,
     module Firebase.JWK.Types,
     module Firebase.JWK.Convert
 ) where
 
+import Firebase.JWK.Store
 import Firebase.JWK.Types
 import Firebase.JWK.Convert
 
